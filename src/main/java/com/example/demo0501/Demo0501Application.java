e2df3cb2e2ed4f33ea6911cc5cbaee6c433b4214
gitName <a@b.c>
2022-01-05 04:47:44 +0700

    first commit

---
+++ src/main/java/com/example/demo0501/Demo0501Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0501Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo0501/Demo0501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0501ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
